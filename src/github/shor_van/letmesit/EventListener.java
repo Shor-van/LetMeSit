92017b0486a3c2de8be6ece00adb5a8a636e8c6f
Shor-van <shorxvan01@gmail.com>
2018-10-18 02:15:29 +0200

    First Commit

---
+++ src/github/shor_van/letmesit/EventListener.java
@@ -0,0 +1,8 @@
+package github.shor_van.letmesit;
+
+import org.bukkit.event.Listener;
+
+public class EventListener implements Listener
+{
+
+}
---
+++ src/github/shor_van/letmesit/LetMeSit.java
@@ -0,0 +1,18 @@
+package github.shor_van.letmesit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class LetMeSit extends JavaPlugin
+{
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+}
